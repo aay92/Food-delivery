ae05138dbe1198a3113584ef1625ea0d82f364a1
Aleksey <aay92@yandex.ru>
2022-10-22 18:59:58 +0400

    Initial Commit

---
+++ Food delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food delivery
+//
+//  Created by Aleksey Alyonin on 22.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
